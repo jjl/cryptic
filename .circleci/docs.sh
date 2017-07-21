e244f35929cfbcb8bbe04e8b7920f8bd4de8c9cc
jjl <james@behemoth>
2017-07-21 17:26:33 +0200

    circleci

--- .circleci/docs.sh
+++ .circleci/docs.sh
@@ -1,3 +1,4 @@
+git checkout -b gh-pages
 apt-get update
 apt-get -y install curl build-essential git-core
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
@@ -7,7 +8,6 @@ export BOOT_AS_ROOT=yes
 ./boot testing
 ./boot codox target
 mv -f target/doc/ ./
-git checkout gh-pages
 find . -mindepth 1 -maxdepth 1 -type f | xargs rm -f
 mv -f doc/* .
 rmdir doc
@@ -15,5 +15,5 @@ git add -A .
 git commit -m "Rebuild website documentation"
 git config user.name "CircleCI"
 git config user.email "docs@circleci"
-openssl aes-256-cbc -d -in docs_rsa.env -out $HOME/.ssh/docs_rsa -k $DOC_KEY_PASSWORD
-git push origin gh-pages
+openssl aes-256-cbc -d -in docs_rsa.env -out $HOME/.ssh/docs_rsa -k $DOCSKEY
+git push -f origin gh-pages
