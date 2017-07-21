642c0d3cdf3837198f291e21b1e1fba8cb20c562
jjl <james@behemoth>
2017-07-21 17:50:56 +0200

    circleci

--- .circleci/docs.sh
+++ .circleci/docs.sh
@@ -11,9 +11,9 @@ mv -f target/doc/ ./
 find . -mindepth 1 -maxdepth 1 -type f | xargs rm -f
 mv -f doc/* .
 rmdir doc
-git add -A .
-git commit -m "Rebuild website documentation"
 git config user.name "CircleCI"
 git config user.email "docs@circleci"
+git add -A .
+git commit -m "Rebuild website documentation"
 openssl aes-256-cbc -d -in docs_rsa.env -out $HOME/.ssh/docs_rsa -k $DOCSKEY
 git push -f origin gh-pages
