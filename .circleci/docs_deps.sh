83c8454a94eed4f85cec15782d473b4f832a1b46
jjl <james@behemoth>
2017-07-21 19:05:41 +0200

    circleci

--- .circleci/docs_deps.sh
+++ .circleci/docs_deps.sh
@@ -2,3 +2,7 @@ apt-get update
 apt-get -y install curl build-essential git-core
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get install -y nodejs
+.circleci/install_boot.sh
+./boot testing
+export BOOT_AS_ROOT=yes
+export BOOT_JVM_OPTIONS=-Xmx3200m
--- .circleci/docs_gen.sh
+++ .circleci/docs_gen.sh
@@ -1,8 +1,6 @@
 git checkout -b gh-pages
-.circleci/install_boot.sh
 export BOOT_AS_ROOT=yes
 export BOOT_JVM_OPTIONS=-Xmx3200m
-./boot testing
 ./boot codox target
 mv -f target/doc/ ./
 find . -mindepth 1 -maxdepth 1 -type f | xargs rm -f
