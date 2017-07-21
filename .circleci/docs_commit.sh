d0a3af3b305e64ca72bc5f6deb9e84da42fbb549
jjl <james@behemoth>
2017-07-21 18:52:01 +0200

    circleci

--- .circleci/docs_commit.sh
+++ .circleci/docs_commit.sh
@@ -1,16 +1,3 @@
-apt-get update
-apt-get -y install curl build-essential git-core
-curl -sL https://deb.nodesource.com/setup_6.x | bash -
-apt-get install -y nodejs
-git checkout -b gh-pages
-.circleci/install_boot.sh
-export BOOT_AS_ROOT=yes
-./boot testing
-./boot codox target
-mv -f target/doc/ ./
-find . -mindepth 1 -maxdepth 1 -type f | xargs rm -f
-mv -f doc/* .
-rmdir doc
 git config user.name "CircleCI"
 git config user.email "docs@circleci"
 git add -A .
---
+++ .circleci/docs_deps.sh
@@ -0,0 +1,4 @@
+apt-get update
+apt-get -y install curl build-essential git-core
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
+apt-get install -y nodejs
