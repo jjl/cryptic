cfbb4483b23f6f661aa5f97061d3c7edd59eb77e
jjl <james@behemoth>
2017-07-22 14:54:03 +0200

    circleci

--- .circleci/docs_deps.sh
+++ .circleci/docs_deps.sh
@@ -1,9 +1,4 @@
-set -e
-apt-get update
-apt-get -y install curl build-essential git-core strace ltrace
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
-apt-get install -y nodejs
+apt-get install -y nodejs build-essential
 .circleci/install_boot.sh
-export BOOT_AS_ROOT=yes
-export BOOT_JVM_OPTIONS=-Xmx3200m
-./boot testing
+BOOT_AS_ROOT=yes BOOT_JVM_OPTIONS=-Xmx3200m ./boot testing
